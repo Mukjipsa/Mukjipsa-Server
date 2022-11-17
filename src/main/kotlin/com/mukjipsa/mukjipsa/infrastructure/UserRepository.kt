28964330d565af9e30ae0f1eb6c1a15cfdc770ae
gunom <gu3062@naver.com>
2022-11-17 21:49:29 +0900

    [FEAT] kakao login service 구현

--- src/main/kotlin/com/mukjipsa/mukjipsa/infrastructure/UserRepository.kt
+++ src/main/kotlin/com/mukjipsa/mukjipsa/infrastructure/UserRepository.kt
@@ -4,4 +4,5 @@ import com.mukjipsa.mukjipsa.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface UserRepository : JpaRepository<User, Int> {
+    fun findBySsoId(ssoId: String): User?
 }
--- src/main/kotlin/com/mukjipsa/mukjipsa/service/AuthService.kt
+++ src/main/kotlin/com/mukjipsa/mukjipsa/service/AuthService.kt
@@ -1,5 +1,10 @@
 package com.mukjipsa.mukjipsa.service
 
+import com.mukjipsa.mukjipsa.service.dto.LoginResponse
+import javax.transaction.Transactional
+
 interface AuthService {
     fun getUserId(): Int
+    @Transactional
+    fun loginWithToken(providerName: String, userToken: String): LoginResponse?
 }
