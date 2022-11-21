b3fb1e27ad33abc15dadf94e55133eec32f77d40
dltjdn <hana40004@naver.com>
2022-11-21 19:44:32 +0900

    [FEAT] RecipeController 구현

--- src/main/kotlin/com/mukjipsa/contoroller/RecipeController.kt
+++ src/main/kotlin/com/mukjipsa/contoroller/RecipeController.kt
@@ -10,24 +10,31 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/recipe")
 class RecipeController(
+        private val recipeFacade: RecipeFacade,
+        private val authService: AuthService,
 ) {
     private val log: Logger = LoggerFactory.getLogger(javaClass)
 
     @GetMapping
     fun getRecipeList(){
         // TODO 레시피 리스트.
-        log.info("recipe list")
+        val userId = authService.getUserId()
+        return bookmarkFacade.getRecipeList(userId)
     }
 
-    @GetMapping("/{id}")
-    fun getRecipe(@PathVariable id: String){
+    @GetMapping("/{recipeId}")
+    fun getRecipe(@PathVariable recipeId: String){
         // TODO 특정 레시피 조회.
-        log.info("recipe")
+        val userId = authService.getUserId()
+        return bookmarkFacade.getRecipe(userId,recipeId)
     }
 
     @GetMapping("/my")
     fun getRecipeForMyIngredient(){
         // TODO 내가 가진 식재료로 만들 수 있는 레시피 리스트.
-        log.info("recipe for my ingredients")
+        val userId = authService.getUserId()
+        return bookmarkFacade.getRecipeList(userId)
     }
 }
+
+
