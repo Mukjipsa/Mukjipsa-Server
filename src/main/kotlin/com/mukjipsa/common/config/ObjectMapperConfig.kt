be43bffe18d18e53ec9a2348661483f27193e4dd
morgan <gu3062@naver.com>
2022-11-23 13:21:28 +0900

    [FIX] java time 직렬화 수정

--- src/main/kotlin/com/mukjipsa/common/config/ObjectMapperConfig.kt
+++ src/main/kotlin/com/mukjipsa/common/config/ObjectMapperConfig.kt
@@ -1,6 +1,7 @@
 package com.mukjipsa.common.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -9,6 +10,9 @@ import org.springframework.context.annotation.Configuration
 class ObjectMapperConfig {
     @Bean
     fun objectMapper(): ObjectMapper {
-        return ObjectMapper().registerModule(JavaTimeModule())
+        val objectMapper = ObjectMapper()
+        objectMapper.registerModule(JavaTimeModule())
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper
     }
 }
