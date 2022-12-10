abbe4678881bba22900e7f7d3e3cb7cd19ce5059
gunom <gu3062@naver.com>
2022-12-10 17:08:25 +0900

    [FEAT] search recipe 임시 완성

--- src/main/kotlin/com/mukjipsa/service/RecipeService.kt
+++ src/main/kotlin/com/mukjipsa/service/RecipeService.kt
@@ -8,5 +8,6 @@ interface RecipeService {
     fun getRecipeByIdIn(recipeIds: List<Int>): List<RecipeDto>
     fun getAllRecipe(): List<Recipe>
     fun getRecipe(recipeId: Int): Optional<Recipe>
+    fun getRecipeByKeyword(keyword: String): List<Recipe>
 
 }
