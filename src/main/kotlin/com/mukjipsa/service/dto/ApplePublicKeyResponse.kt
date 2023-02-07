9f777f56d4ad24f5ff5b8efd052804525ad59ce4
dltjdn <hana40004@naver.com>
2023-02-07 11:18:53 +0900

    [FEAT] ApplePublicKeyResponse dto 생성

---
+++ src/main/kotlin/com/mukjipsa/service/dto/ApplePublicKeyResponse.kt
@@ -0,0 +1,18 @@
+package com.mukjipsa.service.dto
+
+import java.util.*
+
+class ApplePublicKeyResponse(var keys: List<Key>) {
+    data class Key(
+            var kty: String,
+            var kid: String,
+            var use: String,
+            var alg: String,
+            var n: String,
+            var e: String
+    )
+
+    fun getMatchedKeyBy(kid: String, alg: String): Key? {
+        return keys.firstOrNull { it.kid == kid && it.alg == alg }
+    }
+}
