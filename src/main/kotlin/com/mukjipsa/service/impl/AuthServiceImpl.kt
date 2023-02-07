c17b1716456c82c3a0406a87e60e9bc814fad71e
dltjdn <hana40004@naver.com>
2023-02-07 11:20:01 +0900

    [FEAT] apple login 로직 추가

--- src/main/kotlin/com/mukjipsa/service/impl/AuthServiceImpl.kt
+++ src/main/kotlin/com/mukjipsa/service/impl/AuthServiceImpl.kt
@@ -2,22 +2,35 @@ package com.mukjipsa.service.impl
 
 import com.fasterxml.jackson.core.JsonProcessingException
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.google.gson.Gson
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
 import com.mukjipsa.common.authentication.jwt.JwtAuthenticationProvider
+import com.mukjipsa.common.exception.BusinessException
 import com.mukjipsa.common.exception.UserNotFoundException
+import com.mukjipsa.common.exception.response.ErrorCode
+import com.mukjipsa.contoroller.AppleClient
 import com.mukjipsa.domain.User
 import com.mukjipsa.infrastructure.UserRepository
 import com.mukjipsa.service.AuthService
 import com.mukjipsa.service.dto.KakaoProfile
 import com.mukjipsa.service.dto.LoginResponse
+import io.jsonwebtoken.Jwts
 import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient
+import java.math.BigInteger
+import java.nio.charset.Charset
+import java.security.KeyFactory
+import java.security.spec.RSAPublicKeySpec
+import java.util.*
 import javax.transaction.Transactional
-
 
 @Service
 class AuthServiceImpl(
         private val jwtAuthenticationProvider: JwtAuthenticationProvider,
         private val userRepository: UserRepository,
+        private val appleClient: AppleClient,
 ) : AuthService {
     override fun getUserId(): Int {
 //        return (SecurityContextHolder.getContext().authentication as JwtAuthenticationToken).getUserDetails()?.id
@@ -26,9 +39,89 @@ class AuthServiceImpl(
         return 1
     }
 
+
     @Transactional
     override fun loginWithToken(providerName: String, userToken: String): LoginResponse {
-        val user: User = getUserProfileByToken(providerName, userToken)
+
+        var user: User;
+        when(providerName) {
+            "kakao" -> {
+                user = getUserProfileByToken(providerName, userToken);
+            }
+            "apple" -> {
+                val response = appleClient.getAppleAuthPublicKey();
+                val headerOfIdentityToken = userToken.substring(0, userToken.indexOf("."))
+                val header = ObjectMapper().readValue(String(Base64.getDecoder().decode(headerOfIdentityToken), "UTF-8" as Charset), Map::class.java)
+                val key = response.getMatchedKeyBy(header["kid"] as String, header["alg"] as String)
+                        ?: throw BusinessException(ErrorCode.FAILED_TO_FIND_AVALIABLE_RSA)
+
+                val nBytes = Base64.getUrlDecoder().decode(key.n)
+                val eBytes = Base64.getUrlDecoder().decode(key.e)
+
+                val n = BigInteger(1, nBytes)
+                val e = BigInteger(1, eBytes)
+
+                val publicKeySpec = RSAPublicKeySpec(n, e)
+                val keyFactory = KeyFactory.getInstance(key.kty)
+                val publicKey = keyFactory.generatePublic(publicKeySpec)
+
+                val userInfo = Jwts.parserBuilder().setSigningKey(publicKey).build().parseClaimsJws(userToken).body
+                val userInfoObject = JsonParser.parseString(Gson().toJson(userInfo)) as JsonObject
+                val ssoId = userInfoObject["sub"] as String
+                val email = userInfoObject["email"] as String
+                user = User(
+                        email = email,
+                        provider = providerName,
+                        ssoId = ssoId,
+                )
+                userRepository.save(user)
+
+//
+//                val result= StringBuffer();
+//                try {
+//                    val url = URL("https://appleid.apple.com/auth/keys");
+//                    val conn = url.openConnection() as HttpURLConnection
+//                    conn.requestMethod = "GET"
+//                    val br = BufferedReader(InputStreamReader(conn.inputStream));
+//                    var line = "";
+//                    while ((br.readLine().also{ line = it }) != null) {
+//                        result.append(line);
+//                    }
+//                } catch (e: IOException) {
+//                    throw BusinessException(ErrorCode.FAILED_TO_VALIDATE_APPLE_LOGIN);
+//                }
+//
+//                val keys =  JsonParser.parseString(result.toString()) as JsonObject;
+//                val keyArray = keys.get("keys") as JsonArray;
+//
+//                //클라이언트로부터 가져온 identity token String decode
+//                val decodeArray = userToken.split("\\.").toTypedArray();
+//                val header = String(Base64.getDecoder().decode(decodeArray[0]));
+//
+//                //apple에서 제공해주는 kid값과 일치하는지 알기 위해
+//                val kid = (JsonParser.parseString(header) as JsonObject)["kid"] as JsonElement
+//                val alg = (JsonParser.parseString(header) as JsonObject)["alg"] as JsonElement
+//
+//                //써야하는 Element (kid, alg 일치하는 element)
+//                var availableObject: JsonObject? = null;
+//                for (i in 0 until keyArray.size()) {
+//                    val appleObject = keyArray[i] as JsonObject
+//                    val appleKid = appleObject["kid"]
+//                    val appleAlg = appleObject["alg"]
+//                    if (appleKid == kid && appleAlg == alg) {
+//                        availableObject = appleObject
+//                        break
+//                    }
+//                }
+//                if (availableObject == null) throw BusinessException(ErrorCode.FAILED_TO_FIND_AVALIABLE_RSA)
+//
+//                val publicKey = getPublicKey(availableObject)
+
+            }
+            else -> throw BusinessException(ErrorCode.INVALID_PROVIDER_NAME)
+        }
+
+
         val accessToken: String = jwtAuthenticationProvider.generateAccessToken(user.id)
         val refreshToken: String = jwtAuthenticationProvider.generateRefreshToken(user.id)
         return LoginResponse(accessToken, refreshToken)
@@ -70,4 +163,25 @@ class AuthServiceImpl(
             user
         }
     }
+
+
+//    private fun getPublicKey(availableObject: JsonObject): PublicKey {
+//        val nStr = availableObject["n"].toString()
+//        val eStr = availableObject["e"].toString()
+//
+//        val nBytes = Base64.getUrlDecoder().decode(nStr.substring(1, nStr.length - 1))
+//        val eBytes = Base64.getUrlDecoder().decode(eStr.substring(1, eStr.length - 1))
+//
+//        val n = BigInteger(1, nBytes)
+//        val e = BigInteger(1, eBytes)
+//
+//        try {
+//            val publicKeySpec = RSAPublicKeySpec(n, e)
+//            val keyFactory = KeyFactory.getInstance("RSA")
+//            val publicKey = keyFactory.generatePublic(publicKeySpec)
+//            return publicKey
+//        } catch (exception: Exception) {
+//            throw BusinessException(ErrorCode.FAILED_TO_FIND_AVALIABLE_RSA)
+//        }
+//    }
 }
