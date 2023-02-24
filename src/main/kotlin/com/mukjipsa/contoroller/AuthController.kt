f241fcdcf23941b1b901540fdfe6090a8e172c60
gunom <gu3062@naver.com>
2023-02-24 21:22:53 +0900

    [FEAT] Rogin Response 변경

--- src/main/kotlin/com/mukjipsa/contoroller/AuthController.kt
+++ src/main/kotlin/com/mukjipsa/contoroller/AuthController.kt
@@ -4,6 +4,7 @@ import com.mukjipsa.contoroller.dto.LoginRequestDto
 import com.mukjipsa.contoroller.dto.RefreshRequestDto
 import com.mukjipsa.service.AuthService
 import com.mukjipsa.service.dto.LoginResponse
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -15,12 +16,14 @@ class AuthController(
         private val authService: AuthService,
 ) {
     @PostMapping
-    fun login(@RequestBody loginRequestDto: LoginRequestDto): LoginResponse {
-        return authService.loginWithToken(loginRequestDto.provider, loginRequestDto.ssoToken)
+    fun login(@RequestBody loginRequestDto: LoginRequestDto): ResponseEntity<LoginResponse> {
+        val tokenDto = authService.loginWithToken(loginRequestDto.provider, loginRequestDto.ssoToken)
+        return ResponseEntity.ok().body(tokenDto)
     }
 
     @PostMapping("/refresh")
-    fun refresh(@RequestBody refreshRequestDto: RefreshRequestDto): LoginResponse{
-        return authService.refreshWithToken(refreshRequestDto.refreshToken);
+    fun refresh(@RequestBody refreshRequestDto: RefreshRequestDto): ResponseEntity<LoginResponse> {
+        val tokenDto = authService.refreshWithToken(refreshRequestDto.refreshToken)
+        return ResponseEntity.ok().body(tokenDto)
     }
 }
